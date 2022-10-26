f90142993a773e47af65d312d134c5e29c3cbd92
Tina Gosarič <tina.gosaric@gorenje.com>
2022-10-26 11:18:33 +0200

    Adding class X

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        X x = new X(new Scanner(System.in));
+        x.x();
     }
 }
